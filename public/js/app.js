99dfaf7570dc92049f4444623c8967d830dfbf05
NivasDonthireddy <dlnivas@hotmail.com>
2022-09-11 12:08:54 +0530

    setup APP for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -10,7 +10,7 @@ weatherForm.addEventListener('submit',(e)=>{
     e.preventDefault();
     messageOne.textContent='Loading...';
     messageTwo.textContent = '';
-    fetch('http://localhost:3000/weather?address='+search.value)
+    fetch('/weather?address='+search.value)
             .then((response)=>{
                 response.json().then((data)=>{
                     messageOne.textContent='';
